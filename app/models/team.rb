1055c1ac84cd780d0580a354d5ea01b35708d467
Brandon Randall <br.mhcexchange@gmail.com>
2017-12-12 17:21:15 -0700

    removes link, modifies html for teams page

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,16 +1,16 @@
 class Team
 
-  attr_reader :name, :abbrev, :fg_pct, :wins, :losses,
-              :fouls_drawn_pg, :fouls_pg
+  attr_reader :name, :abbrev, :fg_pct, :wins,
+              :losses, :fouls_drawn_pg, :fouls_pg
 
   def initialize(team)
     @name = team["team"]["Name"]
     @abbrev = team["team"]["Abbreviation"]
-    @fg_pct = team["stats"]["FgPct"]
-    @wins = team["stats"]["Wins"]
-    @losses = team["stats"]["Losses"]
-    @fouls_dwawn_pg = team["stats"]["FoulsDrawnPerGame"]
-    @fouls_pg = team["stats"]["FoulsPersPerGame"]
+    @fg_pct = team["stats"]["FgPct"]["#text"]
+    @wins = team["stats"]["Wins"]["#text"]
+    @losses = team["stats"]["Losses"]["#text"]
+    @fouls_dwawn_pg = team["stats"]["FoulsDrawnPerGame"]["#text"]
+    @fouls_pg = team["stats"]["FoulPersPerGame"]["#text"]
   end
 
 end
