a0c6464e6620d51f6d5e643139f57eb982969b91
Brandon Randall <br.mhcexchange@gmail.com>
2017-12-23 17:56:48 -0700

    adds game times

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,9 @@
 class Game
-  attr_reader :id, :away_team, :home_team
+  attr_reader :id, :away_team, :home_team, :time
 
   def initialize(game)
     @id = game["id"]
+    @time = game["time"]
     @away_team = game["homeTeam"]["Name"]
     @home_team = game["awayTeam"]["Name"]
   end
